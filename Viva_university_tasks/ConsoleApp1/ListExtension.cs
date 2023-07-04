07d8aea828a30258efdf133892af3a7c7354f5dd
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-04 16:17:44 +0400

    List Extension method, about  sorting(Task 2)

---
+++ Viva_university_tasks/ConsoleApp1/ListExtension.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        string? name;
+        int age;
+        public Person()
+        {
+            Console.Write("Please input name: ");
+            this.name = Console.ReadLine();
+            Console.Write("Please input age: ");
+            this.age = int.Parse(Console.ReadLine());
+
+        }
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        //inthernet versia
+        /* public int CompareTo(Person comparePerson)
+        {
+            // A null value means that this object is greater.
+            if (comparePerson == null)
+                return 1;
+
+            else
+                return this.age.CompareTo(comparePerson.age);
+        }*/
+        public string Name
+        {
+            get { return name; }
+        }
+        public int Age
+        {
+            get { return age; }
+        }
+    }
+    public static class ListExtension
+    {
+        public static List<Person> SortedByProperty(this List<Person> list)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                for (int j = i + 1; j < list.Count; j++)
+                {
+                    if (list[i].Age > list[j].Age)
+                    {
+                        Person temp = list[i];
+                        list[i] = list[j];
+                        list[j] = temp;
+                    }
+                }
+            }
+            return list;
+        }
+    }
+}
--- Viva_university_tasks/ConsoleApp1/Program.cs
+++ Viva_university_tasks/ConsoleApp1/Program.cs
@@ -1,4 +1,22 @@
-﻿//------------------TASK 1-----------------------
-using ConsoleApp1;
-string str = "barrev hayastan";
-Console.WriteLine(str.StringManipulation());
+﻿using ConsoleApp1;
+
+//------------------TASK 1-----------------------
+/*string str = "barrev hayastan";
+Console.WriteLine(str.StringManipulation());*/
+//------------------TASK 2-----------------------
+List<Person> myList = new List<Person>();
+Console.Write("Please input quantity of persons: ");
+int numberOfPersons = int.Parse(Console.ReadLine());
+//before sorted
+for (int i = 0; i < numberOfPersons; i++)
+{
+    Person person = new Person();
+    myList.Add(person);
+}
+//after sorted
+//myList.Sort(); //inthernet versia
+List<Person> sortedList = myList.SortedByProperty();
+foreach (Person person in sortedList)
+{
+    Console.WriteLine($"{person.Name} is {person.Age} years old.");
+}
