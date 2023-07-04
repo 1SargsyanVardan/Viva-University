a4a820f88fbfeb781a0c2cb9b384c7f49c05c19a
Vardan <vardan.sargsyan.2001@mail.ru>
2023-07-04 16:08:33 +0400

    Create String Extension method(Task1)

---
+++ Viva_university_tasks/ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿//------------------TASK 1-----------------------
+using ConsoleApp1;
+string str = "barrev hayastan";
+Console.WriteLine(str.StringManipulation());
---
+++ Viva_university_tasks/ConsoleApp1/StringManipulation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    public static class StringExtension
+    {
+        public static string StringManipulation(this string str)
+        {
+            if (str == null)
+                return "The string is haven`t arguments";
+            //chatGPT
+            /*TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+            return textInfo.ToTitleCase(str);*/
+
+            //implementation 
+            string[] temp = str.Split(' ');
+            for (int i = 0; i < temp.Length; i++)
+            {
+                char ch = char.ToUpper(temp[i][0]);
+                string otherStr = temp[i].Substring(1).ToLower();
+                temp[i] = ch + otherStr;
+            }
+            string retStr = string.Join(' ', temp);
+            return retStr;
+        }
+    }
+}
